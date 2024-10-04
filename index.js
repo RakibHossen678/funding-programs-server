399e673d817cc302e93a18f42768338a3c749ac0
RakibHossen678 <rh491464@gmail.com>
2024-10-04 15:52:44 +0600

    initial setup created

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 4000;
+
+// middleware
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
