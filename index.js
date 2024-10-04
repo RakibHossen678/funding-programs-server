e17b5846e699cd544157111bdb99494fb4f8fe1d
RakibHossen678 <rh491464@gmail.com>
2024-10-04 16:02:47 +0600

    mongodb setup created

--- index.js
+++ index.js
@@ -13,6 +13,33 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrdje6l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    const programsCollection = client.db("FundingTrail").collection("funding");
+    const usersCollection = client.db("FundingTrail").collection("users");
+
+    // app.get('')
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
